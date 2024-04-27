61b42fc7e4c1498775f90948cc715f7ab6f44b28
anuj <anujsbhuyar@gmail.com>
2024-04-27 17:06:42 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import './App.css';
+import MainContent from './components/MainContent';
+
+function App() {
+  return (
+    <div className="App">
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
